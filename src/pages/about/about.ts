002521bea8c1641140cf374725c31a6b0d95ffe7
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-09-03 21:35:17 -0300

    arrumando about ts

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -28,63 +28,35 @@ export class AboutPage {
 
   }
 
-  goToSingle(compras) {
-    this.navCtrl.push(EditPage, 
-    {'compras' : compras});
-  }
-
-  somando() {
-    var compras = this.dbService.getAll('compras');
-    this.soma = 0;
-    this.compras.forEach(compras => {compras.forEach(compra2 => {this.soma += Number(compra2.payload), console.log(compra2.payload, this.soma)})});
-        
-        
-        return (this.soma);
-        
-  }
-
-  somando2(){
-    var compras = this.dbService.getAll('compras');
-    this.soma = 0;
-    this.compras.forEach(compras => {compras.forEach(compras2 => {if(compras2.title == "Gremio"){return (this.soma += Number(compras2.payload))}})});
-        
-        
-        return (this.soma);
-        
-  }
-
-  comidinha(){
-    var compras = this.dbService.getAll('compras');
-    this.comidas = [];
-    this.compras.forEach(compras => {compras.forEach(compras2 =>{if(compras2.categoria == "comida"){return(this.comidas.push(compras2))}})});
-    return (this.comidas); 
-    
-  }
-  
   remover(key){
-    this.dbService.revome('compras/2018/9',key).then( d => {console.log("removido")});
+    this.dbService.revome('compras',key).then( d => {console.log("removido")});
   }
     
   icon(categoria){
     var icon
     this.categorias.forEach(element => {if (element.title == categoria) {icon = element.icon}});
     return icon
-    
-
   }
 
-  visualOrdem(){
-    var newvis = [];
-    var compras = [];
-    this.visual = this.dbService.getAll('visual')
-    this.visual = this.visual.forEach(visual1 => {visual1.forEach(visual2 => {newvis.push(visual2), compras.push(this.dbService.getAll(['compras',visual2.ano,visual2.mes].join('/')))})})
-    console.log(compras,"olha as compras aqui");
-    newvis.sort(function compare(a,b){
-      if (a.final < b.final) return -1;
-      if (a.final > b.final) return 1;
-      return 0;});
-      return(newvis);
+  goToSingle(compras) {
+    this.navCtrl.push(EditPage, 
+    {'compras' : compras});
   }
+
+
+  somando(){
+    var compras = this.dbService.getAll('compras');
+    this.soma = 0;
+    this.compras.forEach(compras => {compras.forEach(compras2 => {if(compras2.title == "Gremio"){return (this.soma += Number(compras2.payload))}})});
+    return (this.soma);
+        
+  }
+
+  
+  
+
+
+  
 
 
 
