a94d66c31f8308a2c201c793ae0a98abff5aa6db
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-09-03 22:53:03 -0300

    comecando funcoes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -22,8 +22,9 @@ export class AboutPage {
     this.categorias = this.dbService.getArray('categoria')
     this.compras = this.dbService.getAll('compras')
     this.visual = this.dbService.getAll('visual')
-    console.log(this.visual, "visu")
-    console.log(this.compras)
+
+
+    this.DefinindoArrays();
     
 
   }
@@ -52,7 +53,38 @@ export class AboutPage {
         
   }
 
+  DefinindoArrays2(){
+  var compras = this.dbService.getAll('compras')
+  console.log(compras, "esse é o primeiro, o observable, mas pode ser defino com o | async no ngFor = let a of itens| async")
+  compras.forEach(itens => {console.log(itens, "esse é o array com todas as iterações"),
+  itens.forEach(item => {console.log(item), "esse é o item especifico, vai ter varios"})})
+  }
+
+  DefinindoArrays(){
+    var anos = []
+    var meses = []
+    var compras = this.dbService.getAll('compras')
+    console.log(compras)
+    compras.forEach(itens => {
+    itens.forEach(item => {console.log(item,"itens aqui"); if (Boolean(this.verificoSeEstaNaLista(item.ano,anos)) == false) {anos.push(item.ano)}})})
+    console.log(anos,"os anos são")
+    }
+
   
+
+    verificoSeEstaNaLista(item,lista){
+      console.log(item,lista,"olhe esses valores")
+      var contador = 0
+      lista.forEach(element => { if(element == item) {contador =+ 1 }})
+      if (contador > 0){return true}
+      return true
+        
+    
+
+        
+      
+
+    }
   
 
 
