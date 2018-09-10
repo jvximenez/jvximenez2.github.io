149f8486ddb5c92ba406b5d8a56543caf5371811
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-09-10 17:31:24 -0300

    osso

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -9,20 +9,27 @@ import { forkJoin, empty } from 'rxjs';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  
 
   public compras;
+  compras2;
   public comidas;
   public soma;
   public categorias;
+  
 
   public visual;
-  public varredura = [];
+  public varredura;
 
   constructor(public navCtrl: NavController, public dbService: FirebaseServiceProvider) {
     this.categorias = this.dbService.getArray('categoria')
     this.compras = this.dbService.getAll('compras')
     this.visual = this.dbService.getAll('visual')
-    this.DefinindoArrays()
+    this.varredura = this.DefinindoArrays();
+    console.log(this.varredura);
+    
+  
+    
     
     
     
@@ -51,60 +58,62 @@ export class AboutPage {
     this.compras.forEach(compras => {compras.forEach(compras2 => {if(compras2.title == "Gremio"){return (this.soma += Number(compras2.payload))}})});
     return (this.soma);
         
-  }
+  };
 
   DefinindoArrays2(){
   var compras = this.dbService.getAll('compras')
   console.log(compras, "esse é o primeiro, o observable, mas pode ser defino com o | async no ngFor = let a of itens| async")
   compras.forEach(itens => {console.log(itens, "esse é o array com todas as iterações"),
   itens.forEach(item => {console.log(item), "esse é o item especifico, vai ter varios"})})
-  }
+  };
 
   DefinindoArrays(){
     var array = [];
-    var comprasSeparadas;
+    var a =1;
+    var PF;
+    var compras2;
     var varredura = [];
     var compras = this.dbService.getAll('compras');
     compras.forEach(itens => {
-    itens.forEach(item => {if (Boolean(this.verificoSeEstaNaLista(item['ano'], item['mes'],varredura)) == false) {varredura.push(([item['ano'],item['mes']].join(' - ')))}; 
+    itens.forEach(item => {if (Boolean(this.verificoSeEstaNaLista(item['ano'], item['mes'],varredura)) == false) {varredura.push(([item['ano'],item['mes']].join(' - ')))}
     })});
     array.push(varredura);
-    compras.forEach(itens => {console.log(itens,"olha os itens"); comprasSeparadas = this.ListaDeListasComKey(varredura,itens) })
-    console.log("valor da compra separada", Array(comprasSeparadas));
-    array.push(comprasSeparadas)
-    console.log(array, "array final")
-    return(array)
-    };
+    compras.forEach(itens => {if (a==1){ compras2 = this.ListaDeListasComKey(varredura,itens); console.log(compras2,"n entendo"); compras2 = 1; console.log(compras2,"n entendo mesmo"); return (compras2 = 2)}})
+    compras2 = 555
+    console.log(compras2, "olha essa porra aqui")
+    return (compras2);
+  };
     
-    
-    
-
+    quesaco(varredura){
+      var compras = this.dbService.getAll('compras');
+      var comprasSeparadas = [];
+      compras.forEach(itens => { console.log(itens,"olha os itens");  comprasSeparadas = this.ListaDeListasComKey(varredura,itens), console.log(comprasSeparadas,"n entendo")})
+      console.log(comprasSeparadas, "olha essa porra aqui")
+    }
   
 
     verificoSeEstaNaLista(ano,mes,lista){
       var contador = 0;
       var item = ([ano,mes].join(' - '));
-      lista.forEach(element => { if(element == item) {contador =+ 1 }})
+      lista.forEach(element => { if(element == item) {contador =+ 1 }});
       if (contador > 0){return true}
       return false
 
     }
 
     ListaDeListasComKey(lista,itens){
-      var arrayPr = [];
-      console.log(arrayPr,"primeiro array");
-      var arrayL = [];
+      let arrayPr = [];
+      
+
       var cont = 0;
       var adicao;
       while (cont < lista.length) {
-        arrayPr.push(arrayL)
+        arrayPr.push([]);
         cont += 1
-        console.log("esse é o arrayPr sem nada",arrayPr)
-      }
-        console.log("esse é o arrayPr sem nada",arrayPr)
-        itens.forEach(item => {adicao = (this.verificoSeigual(item['ano'],item['mes'],lista));  console.log(adicao,"adicao"); arrayPr = this.PUSH(arrayPr,item,adicao); console.log("esse valor array P de cada iteracao", arrayPr)});
+        };
+        itens.forEach(item => { adicao = (this.verificoSeigual(item['ano'],item['mes'],lista)); arrayPr = this.PUSH(arrayPr,item,adicao);});
         console.log("esse array P completo ", arrayPr);
-        return (arrayPr)
+        return (arrayPr);
 
     }
     
@@ -115,18 +124,13 @@ export class AboutPage {
       lista.forEach(element => {contador += 1; if (element == item) {espera = contador}})
       return (espera);
 
-    }
+    };
 
     PUSH(lista,item,adicao){
-      console.log(lista,item,adicao, "primeira");
-      lista[adicao].push(item);
-      console.log(lista,item,adicao, "segunda");
+      lista[adicao][(lista[adicao].length)]=  Object(item);
       return(lista);
 
-
-
-
-    }
+    };
 
 
   
