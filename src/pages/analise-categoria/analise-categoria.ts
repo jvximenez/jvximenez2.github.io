580a1768d6c5c169ea186903f08d9bf83db0ef37
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-09-17 16:36:30 -0300

    pagina de categorias

--- src/pages/analise-categoria/analise-categoria.ts
+++ src/pages/analise-categoria/analise-categoria.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
+import { EditPage } from '../edit/edit';
 
 
 /**
@@ -21,16 +22,41 @@ export class AnaliseCategoriaPage {
   dataEnviada;
   categoriaEnviada;
   public compras;
+  categorias;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider) {
-    this.compras = this.navParams.get('compras')
+    this.compras = this.navParams.get('compra')
     this.dataEnviada = this.navParams.get('data')
     this.categoriaEnviada = this.navParams.get('categoria')
+    this.categorias = this.dbService.getArray('categoria')
     console.log('ola,', this.dataEnviada, this.compras, this.categoriaEnviada)
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AnaliseCategoriaPage');
+  }
+
+  remover(key){
+    this.dbService.revome('compras',key).then( d => {console.log("removido")});
+  }
+    
+  icon(categoria){
+    var icon
+    this.categorias.forEach(element => {if (element.title == categoria) {icon = element.icon}});
+    return icon
+  }
+
+  goToSingle(compras) {
+    this.navCtrl.push(EditPage, 
+    {'compras' : compras});
+  }
+
+  dia(dia){
+    var retornavel;
+    var fileds = dia.split('/')
+    retornavel = fileds[0];
+    return (retornavel);
+
   }
 
 }
